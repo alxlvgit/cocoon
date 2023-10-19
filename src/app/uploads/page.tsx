dd7966011b755c7d3cf7cb137e5cdb315efed7d3
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-10-19 01:16:20 -0700

    white container added

--- src/app/uploads/page.tsx
+++ src/app/uploads/page.tsx
@@ -119,82 +119,84 @@ function Uploads() {
 
   return (
     <div className="flex flex-col m-16  items-center bg-blue-100 rounded-xl border-2 shadow-2xl  h-screen">
-      <div className=" flex items-center bg-gray-500 w-96 h-40 mt-6 rounded-xl border-2 shadow-lg">
-        <p className="ml-24 text-white text-5xl">Cocoon</p>
+      <div className="relative flex items-center bg-gray-500 w-96 h-40 m-6 rounded-xl border-2 shadow-lg">
+        <p className="ml-24 mb-10 text-white text-5xl">Cocoon</p>
       </div>
 
-      <p className="mb-4 text-lg font-semibold">Upload Your Resume</p>
-      <input
-        className="block w-1/4 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-        type="file"
-        accept="application/pdf,.docx"
-        onChange={(e) => setFile(e.target.files![0])}
-      />
-      <p
-        className="mt-2 w-1/4 text-xs text-left text-white dark:text-gray-300"
-        id="file_input_help"
-      >
-        PDF, DOCX only
-      </p>
-
-      <button
-        onClick={handleFileUpload}
-        className=" mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Upload
-      </button>
-      {...status}
-      {loading && (
-        <>
-          <div className="mt-8 animate-spin rounded-full h-32 w-32 border-b-2 border-black dark:border-white"></div>
-        </>
-      )}
-      <div className="w-3/4 mt-6">
-        {transferableSkills.length > 0 && (
-          <>
-            <p className="text-lg font-semibold m-5">
-              Transitioning to: Web and Digital Interface Designers
-            </p>
-            <p className="text-lg font-semibold m-5">Transferable Skills:</p>
-          </>
-        )}
-        {transferableSkills.map((phrase, index) => (
-          <div key={index} className="flex flex-col mb-4">
-            <p className="text-sm font-semibold">- {phrase}</p>
-          </div>
-        ))}
-        {missingSkills.length > 0 && (
-          <p className="text-lg font-semibold m-5">Missing Skills:</p>
-        )}
-        {missingSkills.map((phrase, index) => (
-          <div key={index} className="flex flex-col mb-4">
-            <p className="text-sm font-semibold">- {phrase}</p>
-          </div>
-        ))}
-      </div>
-      {/* Google Docs Link */}
-      <form
-        className="flex flex-col items-center justify-center"
-        onSubmit={handleSubmit}
-      >
-        <p className="mb-4 text-lg font-semibold">
-          Enter your Google Doc link of the resume
-        </p>
+      <div className="absolute flex flex-col items-center bg-white w-52 h-20 bottom-0 mb-44 rounded-xl">
+        <p className="mb-4 text-base mt-2  ">Upload Resume</p>
         <input
-          className="block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-          type="text"
-          name={googleDocId}
-          placeholder="Enter Google Doc ID"
-          onChange={(e) => setGoogleDocId(e.target.value)}
+          className="block w-1/4 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          type="file"
+          accept="application/pdf,.docx"
+          onChange={(e) => setFile(e.target.files![0])}
         />
+        <p
+          className="mt-2 w-1/4 text-xs text-left text-black dark:text-gray-300"
+          id="file_input_help"
+        >
+          PDF, DOCX only
+        </p>
 
         <button
-          type="submit"
+          onClick={handleFileUpload}
           className=" mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          Submit
+          Upload
         </button>
-      </form>
+        {...status}
+        {loading && (
+          <>
+            <div className="mt-8 animate-spin rounded-full h-32 w-32 border-b-2 border-black dark:border-white"></div>
+          </>
+        )}
+        <div className="w-3/4 mt-6">
+          {transferableSkills.length > 0 && (
+            <>
+              <p className="text-lg font-semibold m-5">
+                Transitioning to: Web and Digital Interface Designers
+              </p>
+              <p className="text-lg font-semibold m-5">Transferable Skills:</p>
+            </>
+          )}
+          {transferableSkills.map((phrase, index) => (
+            <div key={index} className="flex flex-col mb-4">
+              <p className="text-sm font-semibold">- {phrase}</p>
+            </div>
+          ))}
+          {missingSkills.length > 0 && (
+            <p className="text-lg font-semibold m-5">Missing Skills:</p>
+          )}
+          {missingSkills.map((phrase, index) => (
+            <div key={index} className="flex flex-col mb-4">
+              <p className="text-sm font-semibold">- {phrase}</p>
+            </div>
+          ))}
+        </div>
+        {/* Google Docs Link */}
+        <form
+          className="flex flex-col items-center justify-center"
+          onSubmit={handleSubmit}
+        >
+          <p className="mb-4 text-lg font-semibold">
+            Enter your Google Doc link of the resume
+          </p>
+          <input
+            className="block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            type="text"
+            name={googleDocId}
+            placeholder="Enter Google Doc ID"
+            onChange={(e) => setGoogleDocId(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className=" mt-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
