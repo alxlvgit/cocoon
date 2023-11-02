d3503b279415d1ab14112f509d06149e3679a43a
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-11-02 14:15:53 -0700

    MobileNavbar component added

---
+++ src/components/MobileNavbar.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import React, { useState } from "react";
+
+const MobileNavbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div
+      className={`w-full ${
+        isOpen ? "h-full bg-gray-400" : ""
+      } z-50 fixed top-0 left-0 p-4`}
+    >
+      <div className="flex justify-between items-center">
+        {!isOpen && <div className="text-white font-bold text-xl">Logo</div>}
+        <button
+          className="text-white focus:outline-none lg:hidden"
+          onClick={toggleMenu}
+        >
+          {isOpen ? (
+            // Close Icon
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="24"
+              height="24"
+              viewBox="0 0 50 50"
+            >
+              <path d="M 7.7070312 6.2929688 L 6.2929688 7.7070312 L 23.585938 25 L 6.2929688 42.292969 L 7.7070312 43.707031 L 25 26.414062 L 42.292969 43.707031 L 43.707031 42.292969 L 26.414062 25 L 43.707031 7.7070312 L 42.292969 6.2929688 L 25 23.585938 L 7.7070312 6.2929688 z"></path>
+            </svg>
+          ) : (
+            // Hamburger Icon
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"></path>
+            </svg>
+          )}
+        </button>
+      </div>
+      {isOpen && (
+        <div className="mt-40">
+          <a
+            href="/home"
+            className="block text-white py-10 text-center text-2xl"
+          >
+            Home
+          </a>
+          <a
+            href="/about"
+            className="block text-white py-12 text-center text-2xl"
+          >
+            About
+          </a>
+          <a
+            href="/careers"
+            className="block text-white py-16 text-center text-2xl"
+          >
+            Careers
+          </a>
+
+          <a
+            href="/path"
+            className="block text-white py-16 text-center text-2xl"
+          >
+            Path
+          </a>
+          <a
+            href="/profile"
+            className="block text-white py-16 text-center text-2xl"
+          >
+            Profile
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MobileNavbar;
