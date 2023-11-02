38ce51ba450f7829e3852a874f2f6e86f00e11c1
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-11-02 14:16:06 -0700

    profile Icon added to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
   return (
     <nav className="flex flex-row flex-wrap items-center justify-between p-4 m-auto font-bold text-gray-400">
-      <div className="flex items-center">
+      <div className="flex items-center mr-96">
         <Link href="/">Logo</Link>
       </div>
       <div className="flex items-center space-x-9 sm:space-x-20">
@@ -16,13 +18,13 @@ export default function NavBar() {
         <Link href="/careers" className="hover:text-black">
           Careers
         </Link>
-        <Link href="/career-gap" className="hover:text-black">
-          Paths
+        <Link href="/path" className="hover:text-black">
+          Path
         </Link>
       </div>
-      <div className="flex mt-4 sm:mt-0">
+      <div className="flex mt-4 sm:mt-0 ml-96">
         <Link href="/profile" className="hover:text-black">
-          Profile
+          <FontAwesomeIcon icon={faUser} /> 
         </Link>
       </div>
     </nav>
