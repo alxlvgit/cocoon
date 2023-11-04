2528ec920005163e78bea2172f349bfb25ff5e99
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-11-04 16:58:42 -0700

    icon fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
   return (
@@ -24,7 +22,20 @@ export default function NavBar() {
       </div>
       <div className="flex mt-4 ml-16 sm:mt-0">
         <Link href="/profile" className="hover:text-black">
-          <FontAwesomeIcon icon={faUser} />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
         </Link>
       </div>
     </nav>
