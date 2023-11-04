63501dcc3721a78732619d41b2e6a5ea5c9e4e14
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-11-04 16:58:28 -0700

    icon fixed in mobile navbar

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const MobileNavbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -99,7 +97,20 @@ const MobileNavbar: React.FC = () => {
             href="/profile"
             className="flex items-center justify-center text-white text-2xl"
           >
-            <FontAwesomeIcon icon={faUser} className="mr-2 " />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8 mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
             Profile
           </a>
         </div>
