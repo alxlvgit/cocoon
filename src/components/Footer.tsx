047f2f0f306c344fce87ca308d3aea353ef8325c
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-11-15 10:20:24 -0800

    footer made simpler

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,29 +2,10 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow m-4">
+    <footer className="bg-white rounded-lg  m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="#"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <strong className="text-xl font-bold">Cocoon</strong>
-          </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            <li className="mr-4">
-              <Link href="/about" className="hover:text-black">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href="/home" className="hover:text-black">
-                Home
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
+        {/* <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" /> */}
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
           <a href="#" className="hover:underline">
