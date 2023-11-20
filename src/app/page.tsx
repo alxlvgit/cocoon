d9cf01c1f7a882e1e56a37aac90a8810f628464a
selinasein <neulmisscj@gmail.com>
2023-11-19 21:03:59 -0800

    re-doing the landing page according to the updated figma...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Home() {
   return (
     <div>
-      <div className="grid grid-cols-1 px-16 lg:grid-cols-[3fr_1fr] py-12">
+      <div>
         <Image
           src="/assets/AdobeStock_621566683.jpeg"
           alt="Cocoon Landing Page Photo"
@@ -13,16 +13,26 @@ function Home() {
           className="filter brightness-50"
         />
       </div>
-      <div className="grid grid-cols-1 px-16 lg:grid-cols-[3fr_1fr] py-12">
-        <h1 className="text-3xl md:text-8xl text-white font-bold max-w-[40rem] leading-[90%] tracking-[-2px] pb-3 self-start animate-fade-up animate-duration-[2000ms] animate-normal animate-fill-forwards">
-          Guide you to a{" "}
-          <span className="underline decoration-sky-500 ">brighter </span>
-          tomorrow.
+
+      <div className="grid grid-cols-1 p-16 lg:grid-cols-[2fr_2fr] h-1/5">
+        <h1 className="text-3xl md:text-7xl text-white font-bold max-w-[40rem] leading-[90%] tracking-[-2px] pb-3 self-start animate-fade-up animate-duration-[2000ms] animate-normal animate-fill-forwards">
+          <span className="underline decoration-sky-500 ">Cocoon</span> {""}
+          guide you to a brighter tomorrow.
         </h1>
         <div className="text-base text-white leading-[150%] self-end animate-fade-left animate-duration-[2000ms]  animate-normal animate-fill-forwards grid grid-rows-2">
-          <div>
+          <div className="pb-8 w-full flex flex-co p-10 transition-transform transform justify-center items-center animate-fade">
+            <video
+              autoPlay
+              muted
+              loop
+              playsInline
+              controls
+              src="/assets/Ai-generatedCocoonIntro.mp4"
+            ></video>
+          </div>
+          {/* <div>
             <Link
-              href="/home"
+              href="/dashboard"
               className="inline-flex px-3 py-4 md:px-6 md:py-5 text-center items-center justify-center text-base md:text-xl lg:text-3xl font-medium text-white bg-sky-500 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 m-5 md:m-3"
             >
               Start Now
@@ -42,47 +52,42 @@ function Home() {
                 />
               </svg>
             </Link>
-          </div>
-
-          <div>
-            When uncertain about career choices, remember that{" "}
-            <span className="highlight bg-sky-500/30">COCOON</span> is here for
-            you. It&apos;s a dependable compass, guiding you towards new
-            beginnings in your professional journey.
-          </div>
+          </div> */}
         </div>
       </div>
-      <div className="grid lg:grid-cols-2 bg-white pt-10">
-        <div className="pb-8 w-full flex flex-co p-10 transition-transform transform justify-center items-center animate-fade">
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            controls
-            src="/assets/Ai-generatedCocoonIntro.mp4"
-          ></video>
-        </div>
-        <div className="m-3 w-6/7 h-5/6 flex flex-co p-10 transition-transform transform justify-center items-center pt-10 lg:pt-40">
-          <div className="animate-fade-left bg-white p-7">
-            Cocoon, a pioneering AI-driven company, is revolutionizing the job
-            search and recruitment landscape by harnessing the power of
-            artificial intelligence. By meticulously scanning resumes and
-            extracting essential skills, Cocoon is empowering individuals to
-            find their dream jobs while assisting companies in discovering the
-            ideal candidates. This innovative platform efficiently matches job
-            seekers with career opportunities that align with their expertise,
-            experience, and aspirations. By using AI to bridge the gap between
-            job seekers and employers, Cocoon not only streamlines the job hunt
-            process but also enhances the chances of a perfect fit for both
-            parties. In an era where job markets are rapidly evolving,
-            Cocoon&apos;s AI-driven approach ensures that individuals can
-            seamlessly transition into new roles and contribute their skills to
-            companies seeking top talent.
+      {/* <div className="grid lg:grid-cols-2 bg-white pt-10"> */}
+
+      <div className="m-3 w-6/7 h-5/6 flex flex-co p-10 transition-transform transform justify-center items-center pt-10 lg:pt-40">
+        <div className="animate-fade-left p-7 bg-[#494949] h-80 relative bottom-32 rounded-lg">
+          <h3 className="text-white font-semibold text-xl">About Cocoon</h3>
+          <div>
+            <img
+              height={500}
+              width={300}
+              className="float-right relative rounded-lg bottom-36 ml-4"
+              src="/assets/about-cocoon.jpg"
+            />
+            <p className="text-gray-200 text-">
+              Cocoon, a pioneering AI-driven company, is revolutionizing the job
+              search and recruitment landscape by harnessing the power of
+              artificial intelligence. By meticulously scanning resumes and
+              extracting essential skills, Cocoon is empowering individuals to
+              find their dream jobs while assisting companies in discovering the
+              ideal candidates. This innovative platform efficiently matches job
+              seekers with career opportunities that align with their expertise,
+              experience, and aspirations. By using AI to bridge the gap between
+              job seekers and employers, Cocoon not only streamlines the job
+              hunt process but also enhances the chances of a perfect fit for
+              both parties. In an era where job markets are rapidly evolving,
+              Cocoon&apos;s AI-driven approach ensures that individuals can
+              seamlessly transition into new roles and contribute their skills
+              to companies seeking top talent.
+            </p>
           </div>
         </div>
       </div>
     </div>
+    // </div>
   );
 }
 
