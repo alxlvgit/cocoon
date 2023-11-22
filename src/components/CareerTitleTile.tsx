6cd0a3717068c94a5b5a908221e522f3795e52a3
farbodtandas <farbod8tandas@gmail.com>
2023-11-22 02:23:06 -0800

    minor spelling

--- src/components/CareerTitleTile.tsx
+++ src/components/CareerTitleTile.tsx
@@ -29,7 +29,7 @@ const CareerTile = ({ career }: { career: CareerData }) => {
             href={`/career/${career.code}`}
             className={`group-hover:block hidden text-black bg-button-bg hover:bg-button-bg-hover focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`}
           >
-            Show Detail
+            Show Details
           </Link>
         </div>
       </div>
