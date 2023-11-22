74a265e1fa8de55302da098b3726a45e0ab70b25
selinasein <neulmisscj@gmail.com>
2023-11-21 20:48:32 -0800

    still fixing the responsive errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,44 @@ function Home() {
           />
         </div>
         <div className="grid mx-14 lg:mx-1 md:p-10 h-96	lg:grid-cols-[2fr_2fr] ">
-          <h1 className=" text-white font-bold max-w-[40rem] leading-[90%] tracking-[-2px] pb-3 self-start animate-fade-up animate-duration-[2000ms] animate-normal animate-fill-forwards">
-            <span className="text-6xl md:text-8xl underline decoration-sky-500 ">
-              Cocoon
-            </span>
-            <span className="text-5xl md:text-6xl">
-              {" "}
-              guides you to a brighter tomorrow.
-            </span>
-          </h1>
+          <div>
+            <h1 className=" text-white font-bold max-w-[40rem] leading-[90%] tracking-[-2px] pb-3 self-start animate-fade-up animate-duration-[2000ms] animate-normal animate-fill-forwards">
+              <span className="text-6xl md:text-8xl underline decoration-sky-500 ">
+                Cocoon
+              </span>
+              <span className="text-5xl md:text-6xl">
+                {" "}
+                guides you to a brighter tomorrow.
+              </span>
+            </h1>
+            <div className="relative pt-8">
+              <Link
+                href="/profile"
+                className="relative inline-flex items-center justify-center p-4 px-3 md:px-6 py-2 md:py-3 overflow-hidden font-medium transition duration-300 ease-out border-2 rounded-full shadow-md group border-white"
+              >
+                <span className="absolute inset-0 flex items-center justify-center w-full h-full text-black duration-300 -translate-x-full bg-356CBE group-hover:translate-x-0 ease">
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    ></path>
+                  </svg>
+                </span>
+                <span className="md:text-lg absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">
+                  Start Now
+                </span>
+                <span className="text-lg relative invisible">Start Now</span>
+              </Link>
+            </div>
+          </div>
 
           <div className="text-base text-white leading-[150%] self-end animate-fade-left animate-duration-[2000ms]  animate-normal animate-fill-forwards grid  xl:relative xl:bottom-28">
             <div className="pb-8 w-full p-10 transition-transform transform justify-center items-center animate-fade">
@@ -36,40 +65,6 @@ function Home() {
                 controls
                 src="/assets/Ai-generatedCocoonIntro.mp4"
               ></video>
-            </div>
-            <div className="h-24">
-              {/* <Link
-                href="/dashboard"
-                className="inline-flex relative
-                lg:right-32
-                px-10 py-7
-                md:px-6 md:py-4
-                text-center items-center justify-center
-                text-lg font-medium lg:text-white text-black
-                bg-[#6DB8C5] lg:bg-transparent
-                rounded-full hover:bg-blue-800
-                focus:ring-4 focus:outline-none focus:ring-blue-300
-                dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
-                m-8 md:m-3
-                border"
-              >
-                Start Now
-                <svg
-                  className="w-3.5 h-3.5 ml-1.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </Link> */}
             </div>
           </div>
         </div>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,9 @@
-
 import React from "react";
 import { auth, signOut } from "@/auth";
 import { redirect } from "next/navigation";
 import SignoutButton from "@/components/SignoutButton";
 import ProfileClient from "@/components/ProfileClient";
 import Image from "next/image";
-
 
 /*
 NOTES FROM SELINA TO XIAO 👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽
@@ -15,7 +13,6 @@ Data we need for this page: Missing skills, Matched skills, Number Completed Cou
 If don't have these data, I think alex and you (BE devs) should talk to the designers or 
 at minimum, let's discuss what we have, and what we are going to show on this page....! 
 */
-
 
 export default async function Profile() {
   const session = await auth();
@@ -29,9 +26,8 @@ export default async function Profile() {
       <div className="flex flex-col xs:flex-row xs:justify-between align-middle items-center sm:col-span-2">
         <div className="w-20 h-20 xs:mr-6 mx-auto">
           <Image
-            src={user.image ? user.image : "/images/avatar.png"} 
+            src={user.image ? user.image : "/images/avatar.png"}
             alt={user.name + " avatar"}
-
             width={80}
             height={80}
             className="object-cover h-20 w-20 rounded-full"
@@ -40,9 +36,7 @@ export default async function Profile() {
         <div className="flex-grow flex flex-col sm:flex-row justify-between">
           <div className="mb-2 xs:mb-0 flex flex-col items-start xs:items-start">
             <h1 className="font-bold w-full text-xl">Welcome,</h1>
-            <p className="text-lg text-gray-500 text-left">
-              {user.name}
-            </p>
+            <p className="text-lg text-gray-500 text-left">{user.name}</p>
           </div>
           <SignoutButton
             signOut={async () => {
@@ -54,5 +48,5 @@ export default async function Profile() {
       </div>
       <ProfileClient user={user} />
     </div>
-  )
+  );
 }
