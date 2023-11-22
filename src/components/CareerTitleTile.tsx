86002dd7bb57ce4ce0eda55bf7a8b8ba82060f67
Xiao Zhang <xxxiao971006@gmail.com>
2023-11-22 12:59:27 -0800

    fix hover issues

--- src/components/CareerTitleTile.tsx
+++ src/components/CareerTitleTile.tsx
@@ -17,14 +17,16 @@ const CareerTile = ({ career }: { career: CareerData }) => {
   // };
 
   return (
-    <div className="mx-auto p-4 w-full lg:w-4/5 justify-center items-center align-middle rounded-2xl shadow-xl bg-gradient-to-t from-indigo-300 cursor-pointer hover:bg-indigo-400 z-30 border-0 text-black">
-      <div className="bg-indigo-100 flex flex-col h-full w-full rounded-lg mx-auto p-3 text-center shadow-2xl align-middle items-center justify-center">
+    // <div className="mx-auto p-4 w-full lg:w-4/5 justify-center items-center align-middle rounded-2xl shadow-xl bg-gradient-to-t from-indigo-300 cursor-pointer hover:bg-indigo-400 z-30 border-0 text-black">
+    //   <div className="bg-indigo-100 flex flex-col h-full w-full rounded-lg mx-auto p-3 text-center shadow-2xl align-middle items-center justify-center">
+    <div className="group m-3 mx-auto p-4 w-full lg:w-4/5 justify-center items-center align-middle rounded-2xl bg-custom-gradient shadow-custom-shadow cursor-pointer z-30 border-0 text-black hover:bg-custom-gradient-dark">
+      <div className="bg-custom-bg flex flex-col h-full w-full rounded-lg mx-auto p-1 text-center shadow-custom-shadow align-middle items-center justify-center ">
         <div>
-          <h1 className="text-md md:text-base lg:text-lg text-center w-full">
+          <h1 className="text-md md:text-base lg:text-lg text-center w-full mt-2 ">
             {career.title}
           </h1>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 pt-5 lg:gap-4 gap-1">
+        {/* <div className="grid grid-cols-1 lg:grid-cols-2 pt-5 lg:gap-4 gap-1">
           <Link
             href={`/uploads/${career.code}`}
             id={career.code}
@@ -32,13 +34,28 @@ const CareerTile = ({ career }: { career: CareerData }) => {
                bg-gray-500 hover:bg-gray-900 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`}
           >
             Start Analysis
-          </Link>
-          <Link
-            href={`/career/${career.code}`}
-            className={` text-white bg-gray-500 hover:bg-gray-900 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`}
-          >
-            Show Detail
-          </Link>
+          </Link> */}
+
+        <div className="flex flex-cols-2">
+        {/* invisible group-hover:visible */}
+          <div className=" invisible group-hover:visible grid grid-cols-1 lg:grid-cols-1 pt-2 lg:gap-4 gap-1 ">
+            <Link
+              href={`/career/${career.code}`}
+              className={`  text-black bg-button-bg hover:bg-button-bg-hover focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`}
+              // className={`group-hover:block hidden text-black bg-button-bg hover:bg-button-bg-hover focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`}
+            >
+              Show Detail
+            </Link>
+          </div>
+          <div className=" invisible group-hover:visible grid grid-cols-1 lg:grid-cols-1 pt-2 lg:gap-4 gap-1 ">
+            <Link
+              href={`/uploads/${career.code}`}
+              // className={`  text-black bg-button-bg hover:bg-button-bg-hover focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`}
+            className={`group-hover:block hidden text-black bg-button-bg hover:bg-button-bg-hover focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`}
+            >
+              Start 
+            </Link>
+          </div>
         </div>
       </div>
     </div>
