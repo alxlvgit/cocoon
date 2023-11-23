6b077b8a19c6d7007957a3d0ff6719976a993d3d
Oleksandr Levchuk <73852716+alxlvgit@users.noreply.github.com>
2023-11-23 02:34:48 -0800

    remove not used component

--- src/components/ProfileClient.tsx
+++ src/components/ProfileClient.tsx
@@ -1,8 +1,6 @@
 "use client";
 import TopCareerSuggestions from "@/components/TopCareerSuggestions";
 import SkillsProgress from "@/components/SkillsProgress";
-import MatchedSkills from "@/components/MatchedSkills";
-import SkillsContainer from "@/components/SkillsContainer";
 import SavedCareers from "@/components/SavedCareers";
 
 export default function ProfileClient({ user }: { user: any }) {
