bfb3776f35d3a2effa539ac9a12e44abeb370d2b
Selina Se In <122313816+selinasein@users.noreply.github.com>
2023-11-23 20:35:10 -0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
         'custom-bg': '#C3EBF2',
         'button-bg': '#DBC2CF',
         'button-bg-hover': '#9E8B9A',
-        'main-bg': '#6DB8C5'
+        'main-bg': '#6DB8C5',
         "bright-main": "#D0F0F6",
       },
       colors: {
