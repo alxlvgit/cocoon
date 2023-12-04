6b3c5f98ce7c014494980cfb67d21563ac18c2c7
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-12-03 18:00:24 -0800

    fixed the link position

--- src/components/CareerTitleTile.tsx
+++ src/components/CareerTitleTile.tsx
@@ -2,17 +2,15 @@ import Link from "next/link";
 
 const CareerTile = ({ career }: { career: CareerData }) => {
   return (
-    <div className="group mx-auto p-4 w-full lg:w-full max-w-xs justify-center items-center align-middle rounded-2xl bg-custom-gradient relative shadow-custom-shadow cursor-pointer z-30 border-0 text-black hover:bg-custom-gradient-dark">
-      <div className="bg-custom-bg flex flex-col h-full w-full rounded-lg mx-auto p-2 text-center shadow-custom-shadow align-middle items-center justify-center ">
-        <Link href={`/career/${career.code}`}>
-          <div>
-            <h1 className="text-md md:text-base  lg:text-lg text-center w-full">
-              {career.title}
-            </h1>
-          </div>
-        </Link>
+    <Link href={`/career/${career.code}`}>
+      <div className="group mx-auto p-4 w-full lg:w-full max-w-xs justify-center items-center align-middle rounded-2xl bg-custom-gradient relative shadow-custom-shadow cursor-pointer z-30 border-0 text-black hover:bg-custom-gradient-dark">
+        <div className="bg-custom-bg flex flex-col h-full w-full rounded-lg mx-auto p-2 text-center shadow-custom-shadow align-middle items-center justify-center ">
+          <h1 className="text-md md:text-base lg:text-lg text-center w-full">
+            {career.title}
+          </h1>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
