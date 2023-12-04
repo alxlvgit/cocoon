2136e8ca933ae9857a0d15ad0ad43183637abfa2
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-12-03 18:03:38 -0800

    fixed thee size for titles

--- src/components/CareerTitleTile.tsx
+++ src/components/CareerTitleTile.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 const CareerTile = ({ career }: { career: CareerData }) => {
   return (
     <Link href={`/career/${career.code}`}>
-      <div className="group mx-auto p-4 w-full lg:w-full max-w-xs justify-center items-center align-middle rounded-2xl bg-custom-gradient relative shadow-custom-shadow cursor-pointer z-30 border-0 text-black hover:bg-custom-gradient-dark">
-        <div className="bg-custom-bg flex flex-col h-full w-full rounded-lg mx-auto p-2 text-center shadow-custom-shadow align-middle items-center justify-center ">
+      <div className="group mx-auto p-4 w-full lg:w-full max-w-xs justify-center items-center align-middle rounded-2xl bg-custom-gradient relative shadow-custom-shadow cursor-pointer z-30 border-0 text-black hover:bg-custom-gradient-dark h-48">
+        {" "}
+        {/* Setting a fixed height */}
+        <div className="bg-custom-bg flex flex-col h-full w-full rounded-lg mx-auto p-2 text-center shadow-custom-shadow align-middle items-center justify-center">
           <h1 className="text-md md:text-base lg:text-lg text-center w-full">
             {career.title}
           </h1>
