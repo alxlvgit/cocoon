ea59e5305098209992495495794b7a03c78fbe32
Xiao Zhang <xxxiao971006@gmail.com>
2023-12-05 01:15:18 -0800

    sth wrong with the text center

--- src/components/CheckDonut.tsx
+++ src/components/CheckDonut.tsx
@@ -100,6 +100,7 @@ export default function CheckDonutCourse({val, currentPathCoursesAndPrograms }: 
 
     return (
         <>
+            <p>{currentCoursePercentage}</p>
             {/* <p>val:{JSON.stringify(currentPathCoursesAndPrograms.skills.filter(skill => skill.acquired === true))}</p> */}
             <SkillsModal
                 key={val || ""}
--- src/components/SkillsModal.tsx
+++ src/components/SkillsModal.tsx
@@ -65,7 +65,6 @@ export default function SkillsModal({
     dispatch(
       checkIfAllSkillsAcquired({ skills: updatedSkillsAcquired, title })
     );
-
     
   };
 
